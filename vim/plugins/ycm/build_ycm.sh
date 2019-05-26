2780d91d8674264a878297aa78c3fe04c1f547a8
Yichi Zhang <ichicho@keio.jp>
2019-05-26 12:18:51 +0900

    bug fixed

--- vim/plugins/ycm/build_ycm.sh
+++ vim/plugins/ycm/build_ycm.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+mkdir -p ~/.vim/pack/plugins/start
+cd ~/.vim/pack/plugins/start
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe/
+git submodule update --init --recursive
+
 cd ~
 mkdir ycm_build
 cd ycm_build
-cmake -G "Unix Makefiles" . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
-      -DPATH_TO_LLVM_ROOT=/opt/clang+llvm \ 
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/opt/clang+llvm . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
       -DPYTHON_INCLUDE_DIR=/usr/local/include/python3.7m \
       -DPYTHON_LIBRARY=/usr/local/lib/libpython3.7m.so.1.0 \
       -DUSE_PYTHON2=OFF \
