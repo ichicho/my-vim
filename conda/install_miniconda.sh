4f681b0974a7ae8e0e0e10b3929bb6fdbc0110ff
Yichi Zhang <ichicho@keio.jp>
2019-05-27 23:44:58 +0900

    bug fixed

--- conda/install_miniconda.sh
+++ conda/install_miniconda.sh
@@ -4,5 +4,5 @@ curl -o ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Lin
 chmod +x ~/miniconda.sh
 ~/miniconda.sh -b -p $HOME/miniconda
 rm ~/miniconda.sh
-# export PATH="$CONDA_PREFIX/bin:$PATH"
+export PATH="$CONDA_PREFIX/bin:$PATH"
 conda update conda
