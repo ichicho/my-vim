649ce9b5c01e8e2206cd4d3ae227da8259229cca
Yichi Zhang <ichicho@keio.jp>
2019-05-28 18:36:30 +0900

    fix

--- vim/build_vim.sh
+++ vim/build_vim.sh
@@ -3,14 +3,16 @@
 cd ~
 git clone https://github.com/vim/vim.git
 
-cd vim
+cd ~/vim
 ./configure --with-features=huge \
             --enable-multibyte \
+	    --with-local-dir=$CONDA_PREFIX \
             --enable-python3interp=yes \
-            --with-python3-command=/usr/local/bin/python3.7 \
-            --with-python3-config-dir=/usr/local/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+            --with-python3-command=$CONDA_PREFIX/bin/python3.7 \
+            --with-python3-config-dir=$CONDA_PREFIX/lib/python3.7/config-3.7m-x86_64-linux-gnu \
             --enable-cscope \
-            LDFLAGS="-Wl,--rpath=/usr/local/lib"
+	    --prefix=$CONDA_PREFIX \
+            LDFLAGS="-Wl,--rpath=$CONDA_PREFIX/lib"
 make -j$(nproc)
 sudo make install -j$(nproc)
 
