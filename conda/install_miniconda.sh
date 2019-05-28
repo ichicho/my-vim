3149f4a4cd03257d2a728d47f6be92f9a5353531
Yichi Zhang <ichicho@keio.jp>
2019-05-28 17:27:54 +0900

    debuged

--- conda/install_miniconda.sh
+++ conda/install_miniconda.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-PYTHON_VERSION=3.7.3
-CONDA_PREFIX=$HOME/miniconda
+python_version=3.7.3
+conda_prefix=$HOME/miniconda
 
 curl -o ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ~/miniconda.sh
-~/miniconda.sh -b -p $CONDA_PREFIX
+~/miniconda.sh -b -p $conda_prefix
 rm ~/miniconda.sh
-$CONDA_PREFIX/bin/conda update conda
 
-$CONDA_PREFIX/bin/conda create -y --name myenv python=$PYTHON_VERSION
-$CONDA_PREFIX/bin/conda init bash
+export PATH="$HOME/miniconda/bin:$PATH"
+conda init bash
+
+conda update conda
+
+conda create -y --name myenv python=$python_version
