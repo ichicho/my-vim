a1a723f15f5122a9b345e9122b30ccaa1a56bd43
Yichi Zhang <ichicho@keio.jp>
2019-05-30 15:58:31 +0900

    conda build fix

--- vim/plugins/ycm/build_ycm.sh
+++ vim/plugins/ycm/build_ycm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 conda install -y cmake
+cd ~
+curl -O https://raw.githubusercontent.com/AnacondaRecipes/libnetcdf-feedstock/master/recipe/cross-linux.cmake
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="$HOME/cross-linux.cmake")
 
 mkdir -p ~/.vim/pack/plugins/start
 cd ~/.vim/pack/plugins/start
@@ -11,11 +14,18 @@ git submodule update --init --recursive
 cd ~
 mkdir ycm_build
 cd ycm_build
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/opt/clang+llvm . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
+# cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/opt/clang+llvm . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
+#       -DPYTHON_INCLUDE_DIR=$CONDA_PREFIX/include/python3.7m \
+#       -DPYTHON_LIBRARY=$CONDA_PREFIX/lib/libpython3.7m.so.1.0 \
+#       -DUSE_PYTHON2=OFF \
+#       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G "Unix Makefiles" . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
+      -DPATH_TO_LLVM_ROOT=/opt/clang+llvm \
       -DPYTHON_INCLUDE_DIR=$CONDA_PREFIX/include/python3.7m \
       -DPYTHON_LIBRARY=$CONDA_PREFIX/lib/libpython3.7m.so.1.0 \
       -DUSE_PYTHON2=OFF \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      ${CMAKE_PLATFORM_FLAGS[@]}
 cmake --build . --target ycm_core
 cp compile_commands.json ~/compile_commands.json
 rm -rf ~/ycm_build
