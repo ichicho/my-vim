3ac093d89effdd3ffc2a57ec5a676dfcf98e4d92
Yichi Zhang <ichicho@keio.jp>
2019-06-04 21:02:30 +0900

    bug fixed

--- base_c/install_clang+llvm.sh
+++ base_c/install_clang+llvm.sh
@@ -2,6 +2,7 @@
 
 cd ~
 curl -OL http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+sudo apt install xz-utils
 tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 cd clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
 sudo cp -r * /usr/local/ 
