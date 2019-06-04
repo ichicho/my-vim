d595c1c569d4d3fcee395e113d9c9ca0e8f9bc42
Yichi Zhang <ichicho@keio.jp>
2019-06-04 20:37:10 +0900

    install clang+llvm at first

---
+++ base_c/install_clang+llvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~
+curl -OL http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+cd clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
+cp -r * /usr/local/ 
+
+rm ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+rm -r ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
