19cfefaad8d60e2f1146d307cacb2a4df055fd6f
Yichi Zhang <ichicho@keio.jp>
2019-06-06 14:52:13 +0900

    fix

--- vim/plugins/ycm/install_cmake.sh
+++ vim/plugins/ycm/install_cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~
-curl -O https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.sh
+curl -OL https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.sh
 chmod +x ~/cmake-3.14.5-Linux-x86_64.sh
 ~/cmake-3.14.5-Linux-x86_64.sh --prefix=/usr/local --skip-license
 
