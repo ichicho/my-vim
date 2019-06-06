f8bd3e6ac9ae20a41d844dd3b2aea23bb88f203c
Yichi Zhang <ichicho@keio.jp>
2019-06-06 15:15:35 +0900

    fix

--- base_c/install_clang+llvm.sh
+++ base_c/install_clang+llvm.sh
@@ -8,9 +8,9 @@ sudo apt install -y --no-install-recommends \
 
 cd ~
 curl -OL http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-cd clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
-sudo cp -r * /usr/local/ 
+tar xf ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+cd ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
+sudo cp -r * /usr/local 
 
 rm ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 rm -r ~/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
--- conda_python/install_miniconda.sh
+++ conda_python/install_miniconda.sh
@@ -2,6 +2,9 @@
 
 conda_install_prefix=$HOME/miniconda
 
+sudo apt update
+sudo apt install -y --no-install-recommends curl
+
 curl -o ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ~/miniconda.sh
 ~/miniconda.sh -b -p $conda_install_prefix
