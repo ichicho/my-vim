346dd21e2451be5c3300cb15c95fbf5439e7ba3a
Yichi Zhang <ichicho@keio.jp>
2019-06-06 14:09:46 +0900

    Added vim build

--- vim/build_vim.sh
+++ vim/build_vim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-conda install -y gxx_linux-64 \
-	         make
-
 cd ~
 git clone https://github.com/vim/vim.git
 
@@ -11,11 +8,10 @@ cd ~/vim
             --enable-multibyte \
 	    --with-local-dir=$CONDA_PREFIX \
             --enable-python3interp=yes \
-            --with-python3-command=$CONDA_PREFIX/bin/python3.7 \
-            --with-python3-config-dir=$CONDA_PREFIX/lib/python3.7/config-3.7m-x86_64-linux-gnu \
             --enable-cscope \
 	    --prefix=$CONDA_PREFIX \
             LDFLAGS="-Wl,--rpath=$CONDA_PREFIX/lib"
+# --with-python3-config-dir=`python -c "import sysconfig;print(sysconfig.get_config_var('srcdir'))"` \
 make -j$(nproc)
 make install -j$(nproc)
 
