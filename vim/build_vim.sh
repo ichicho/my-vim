d7d486249bd924f7382d75a5ddca86951a996771
Yichi Zhang <ichicho@keio.jp>
2019-06-06 14:19:19 +0900

    change

--- vim/build_vim.sh
+++ vim/build_vim.sh
@@ -6,12 +6,11 @@ git clone https://github.com/vim/vim.git
 cd ~/vim
 ./configure --with-features=huge \
             --enable-multibyte \
-	    --with-local-dir=$CONDA_PREFIX \
             --enable-python3interp=yes \
             --enable-cscope \
 	    --prefix=$CONDA_PREFIX \
             LDFLAGS="-Wl,--rpath=$CONDA_PREFIX/lib"
-# --with-python3-config-dir=`python -c "import sysconfig;print(sysconfig.get_config_var('srcdir'))"` \
+# --with-local-dir=$CONDA_PREFIX \
 make -j$(nproc)
 make install -j$(nproc)
 
