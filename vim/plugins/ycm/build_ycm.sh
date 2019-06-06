34427d2e3a513450f74b8fd9ad071ffd6e102243
Yichi Zhang <ichicho@keio.jp>
2019-06-06 14:49:45 +0900

    fix

--- vim/plugins/ycm/build_ycm.sh
+++ vim/plugins/ycm/build_ycm.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-conda install -y cmake
-
-# Anaconda CMake patch
-cp $(dirname $(readlink -f $0))/cross-linux.cmake ~/
-CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="$HOME/cross-linux.cmake")
-
 # Download YouCompleteMe
 mkdir -p ~/.vim/pack/plugins/start
 cd ~/.vim/pack/plugins/start
@@ -18,12 +12,11 @@ mkdir ~/ycm_build
 cd ~/ycm_build
 
 cmake -G "Unix Makefiles" . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
-      -DPATH_TO_LLVM_ROOT=$CONDA_PREFIX \
+      -DUSE_SYSTEM_LIBCLANG=ON \
       -DPYTHON_INCLUDE_DIR=$CONDA_PREFIX/include/python3.7m \
       -DPYTHON_LIBRARY=$CONDA_PREFIX/lib/libpython3.7m.so.1.0 \
       -DUSE_PYTHON2=OFF \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-      ${CMAKE_PLATFORM_FLAGS[@]}
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build . --target ycm_core
 cp compile_commands.json ~/compile_commands.json
 rm -r ~/ycm_build
