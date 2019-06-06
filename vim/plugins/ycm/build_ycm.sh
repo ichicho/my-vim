ecebd3e9cdbf07636b42e50eb3f4df52d42e6219
Yichi Zhang <ichicho@keio.jp>
2019-06-06 15:21:57 +0900

    test

--- vim/plugins/ycm/build_ycm.sh
+++ vim/plugins/ycm/build_ycm.sh
@@ -13,10 +13,11 @@ cd ~/ycm_build
 
 cmake -G "Unix Makefiles" . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
       -DUSE_SYSTEM_LIBCLANG=ON \
-      -DPYTHON_INCLUDE_DIR=$CONDA_PREFIX/include/python3.7m \
-      -DPYTHON_LIBRARY=$CONDA_PREFIX/lib/libpython3.7m.so.1.0 \
-      -DUSE_PYTHON2=OFF \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+#       -DPYTHON_INCLUDE_DIR=$CONDA_PREFIX/include/python3.7m \
+#       -DPYTHON_LIBRARY=$CONDA_PREFIX/lib/libpython3.7m.so.1.0 \
+#       -DUSE_PYTHON2=OFF \
 cmake --build . --target ycm_core
 cp compile_commands.json ~/compile_commands.json
 rm -r ~/ycm_build
