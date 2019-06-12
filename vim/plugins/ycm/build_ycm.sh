31f6a7dbd7c217ce0667329d6755a8090043ff18
Yichi Zhang <ichicho@keio.jp>
2019-06-12 23:14:33 +0900

    Update for torch

--- vim/plugins/ycm/build_ycm.sh
+++ vim/plugins/ycm/build_ycm.sh
@@ -12,12 +12,9 @@ mkdir ~/ycm_build
 cd ~/ycm_build
 
 cmake -G "Unix Makefiles" . ~/.vim/pack/plugins/start/YouCompleteMe/third_party/ycmd/cpp \
-      -DUSE_SYSTEM_LIBCLANG=ON \
       -DUSE_PYTHON2=OFF \
       -DPYTHON_INCLUDE_DIR=`python -c "import sysconfig;print(sysconfig.get_config_var('INCLUDEPY'))"` \
-      -DPYTHON_LIBRARY=`python -c "import sysconfig;print(sysconfig.get_config_var('LIBDIR')+'/'+sysconfig.get_config_var('LDLIBRARY'))"` \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+      -DPYTHON_LIBRARY=`python -c "import sysconfig;print(sysconfig.get_config_var('LIBDIR')+'/'+sysconfig.get_config_var('LDLIBRARY'))"`
 
 cmake --build . --target ycm_core
-cp compile_commands.json ~/compile_commands.json
 rm -r ~/ycm_build
