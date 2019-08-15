52b02b2b11180b51110f482b7e7b96fde769e6d0
Yichi Zhang <32590494+ichicho@users.noreply.github.com>
2019-08-15 20:05:18 +0900

    change vim plugin name

--- vim/plugins/fugitive/fugitive.sh
+++ vim/plugins/fugitive/fugitive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/pack/plugins/start
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/plugins/start/fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/plugins/start/vim-fugitive
